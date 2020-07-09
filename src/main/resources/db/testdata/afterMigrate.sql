4ae1b0a5c7c314915dbe51a16c4edb1ecc396271
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2020-07-08 21:00:48 -0300

    More data.

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -107,3 +107,33 @@ values (2, UUID_TO_BIN(UUID()), 4, 1, 2, 1, '38400-111', 'Rua Acre', '300', 'Cas
 
 insert into tb_order_item (order_item_id, purchase_id, product_id, amount, unit_price, total_price, note)
 values (3, 2, 6, 1, 79, 79, 'Ao ponto');
+
+
+insert into tb_purchase (purchase_id, purchase_code, restaurant_id, user_id, payment_id, city_id, address_zip_code,
+                         address_public_place, address_number, address_complement, address_neighborhood,
+                         order_status, creation_date, confirmation_date, cancellation_date, subtotal, shipping_fee, amount)
+values (3, UUID_TO_BIN(UUID()), 1, 1, 1, 1, '38400-222', 'Rua Natal', '200', null, 'Brasil',
+        'ENTREGUE', '2019-10-30 21:10:00', '2019-10-30 21:10:45', '2019-10-30 21:55:44', 110, 10, 120);
+
+insert into tb_order_item (order_item_id, purchase_id, product_id, amount, unit_price, total_price, note)
+values (4, 3, 2, 1, 110, 110, null);
+
+
+insert into tb_purchase (purchase_id, purchase_code, restaurant_id, user_id, payment_id, city_id, address_zip_code,
+                         address_public_place, address_number, address_complement, address_neighborhood,
+                         order_status, creation_date, confirmation_date, cancellation_date, subtotal, shipping_fee, amount)
+values (4, UUID_TO_BIN(UUID()), 1, 2, 1, 1, '38400-800', 'Rua Fortaleza', '900', 'Apto 504', 'Centro',
+        'ENTREGUE', '2019-11-02 20:34:04', '2019-11-02 20:35:10', '2019-11-02 21:10:32', 174.4, 5, 179.4);
+
+insert into tb_order_item (order_item_id, purchase_id, product_id, amount, unit_price, total_price, note)
+values (5, 4, 3, 2, 87.2, 174.4, null);
+
+
+insert into tb_purchase (purchase_id, purchase_code, restaurant_id, user_id, payment_id, city_id, address_zip_code,
+                         address_public_place, address_number, address_complement, address_neighborhood,
+                         order_status, creation_date, confirmation_date, cancellation_date, subtotal, shipping_fee, amount)
+values (5, UUID_TO_BIN(UUID()), 1, 3, 2, 1, '38400-200', 'Rua 10', '930', 'Casa 20', 'Martins',
+        'ENTREGUE', '2019-11-02 21:00:30', '2019-11-02 21:01:21', '2019-11-02 21:20:10', 87.2, 10, 97.2);
+
+insert into tb_order_item (order_item_id, purchase_id, product_id, amount, unit_price, total_price, note)
+values (6, 5, 3, 1, 87.2, 87.2, null);
