70bfb6694b73dda47e3b20f6b32b314dedb7c803
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2020-07-12 06:30:14 -0300

    Validating the file's content type.

--- src/main/java/br/com/fooddelivery/api/dto/entry/PhotoProductEntry.java
+++ src/main/java/br/com/fooddelivery/api/dto/entry/PhotoProductEntry.java
@@ -1,8 +1,10 @@
 package br.com.fooddelivery.api.dto.entry;
 
+import br.com.fooddelivery.core.validation.FileContentType;
 import br.com.fooddelivery.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -13,6 +15,7 @@ import javax.validation.constraints.NotNull;
 public class PhotoProductEntry {
     @NotNull
     @FileSize(max = "500KB")
+    @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     private MultipartFile file;
 
     @NotBlank
