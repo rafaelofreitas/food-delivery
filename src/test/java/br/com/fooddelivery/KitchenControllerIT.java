b8c1ec6efab0a31bddb1fa25dcff63d31eff9550
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2020-08-05 23:00:13 -0300

    Configuring test database.

--- src/test/java/br/com/fooddelivery/KitchenControllerIT.java
+++ src/test/java/br/com/fooddelivery/KitchenControllerIT.java
@@ -2,13 +2,15 @@ package br.com.fooddelivery;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
@@ -16,17 +18,23 @@ import static org.hamcrest.Matchers.hasSize;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class KitchenControllerIT {
     private final static String BASE_PATH = "/kitchens";
 
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
     @Before
     public void setUp(){
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = this.port;
         RestAssured.basePath = BASE_PATH;
+
+        this.flyway.migrate();
     }
 
     @Test
